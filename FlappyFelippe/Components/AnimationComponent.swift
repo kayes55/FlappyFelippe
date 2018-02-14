9a881586e87ca92a95a73b80dace1469524b5eab
Imrul Kayes <imrulkayes@imruls-mac-mini.local>
2018-02-15 04:44:17 +0600

    Video 10 Add AnimationComponent

---
+++ FlappyFelippe/Components/AnimationComponent.swift
@@ -0,0 +1,31 @@
+//
+//  AnimationComponent.swift
+//  FlappyFelippe
+//
+//  Created by Imrul Kayes on 2/15/18.
+//  Copyright © 2018 Imrul Kayes. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class AnimationComponent: GKComponent {
+    
+    let spriteComponent: SpriteComponent
+    var textures: Array<SKTexture> = []
+    
+    init(entity: GKEntity, textures: Array<SKTexture>) {
+        self.textures = textures
+        self.spriteComponent = entity.component(ofType: SpriteComponent.self)!
+        
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        
+    }
+}
